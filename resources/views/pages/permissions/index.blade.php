c3db0c95c50bc17746ba15cc91b2f66a7b3b8dd3
unknown <minhbrots2@gmail.com>
2024-03-01 08:34:36 +0700

    upload screen permissions and roles

--- resources/views/pages/permissions/index.blade.php
+++ resources/views/pages/permissions/index.blade.php
@@ -1,45 +1,61 @@
 @extends('layouts.master')
 
-@section('title', 'Thông tin cá nhân')
+@section('title', 'Điều Chỉnh Quyền Hạn:')
 
 @section('content')
-    <h1 class="mb-3">Laravel 8 User Roles and Permissions Step by Step Tutorial - codeanddeploy.com</h1>
-
     <div class="bg-light p-4 rounded">
-        <h2>Permissions</h2>
-        <div class="lead">
-            Manage your permissions here.
-            <a href="{{ route('permissions.create') }}" class="btn btn-primary btn-sm float-right">Add permissions</a>
+        <h2>Điều Chỉnh Quyền Hạn</h2>
+        <div class="lead d-flex no-block justify-content-between align-items-center mb-3">
+            <p>Quản lý Quyền Truy Cập các nhóm thành viên, thành viên.</p>
+            <a href="{{ route('permissions.create') }}" class="btn btn-success d-flex align-items-center btn-sm float-right">
+                <span class="icon-item-icon me-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil-plus" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4">
+                        </path>
+                        <path d="M13.5 6.5l4 4"></path>
+                        <path d="M16 19h6"></path>
+                        <path d="M19 16v6"></path>
+                    </svg>
+                </span>
+                Thêm quyền
+            </a>
         </div>
-
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col" width="15%">Name</th>
-                    <th scope="col">Guard</th>
-                    <th scope="col" colspan="3" width="1%"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($permissions as $permission)
+        <div class="system-table table-responsive">
+            <table id="simpletable" class="table border text-nowrap customize-table mb-0 align-middle mb-3">
+                <thead>
                     <tr>
-                        <td>{{ $permission->name }}</td>
-                        <td>{{ $permission->guard_name }}</td>
-                        <td><a href="{{ route('permissions.edit', $permission->id) }}" class="btn btn-info btn-sm">Edit</a>
-                        </td>
-                        <td>
-                            {!! Form::open([
-                                'method' => 'DELETE',
-                                'route' => ['permissions.destroy', $permission->id],
-                                'style' => 'display:inline',
-                            ]) !!}
-                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
-                            {!! Form::close() !!}
-                        </td>
+                        <th scope="col" width="15%">Tên quyền</th>
+                        <th scope="col">Vị trí</th>
+                        <th scope="col" colspan="3" width="1%">Thao tác</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-
+                </thead>
+                <tbody>
+                    @foreach ($permissions as $permission)
+                        <tr>
+                            <td>{{ $permission->name }}</td>
+                            <td>{{ $permission->guard_name }}</td>
+                            <td><a href="{{ route('permissions.edit', $permission->id) }}" class="btn btn-info btn-sm">
+                                <span class="icon-item-icon">
+                                    <img
+                                        src="{{ asset('img-system/system/edit_white.svg') }}">
+                                </span>
+                                </a>
+                                <form method="POST" action="{{ route('permissions.destroy', $permission->id) }}" style="display:inline;">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-danger btn-sm">
+                                        <span class="icon-item-icon">
+                                            <img src="{{ asset('img-system/system/trash_white.svg') }}"
+                                                alt="">
+                                        </span>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
--- resources/views/pages/roles/show.blade.php
+++ resources/views/pages/roles/show.blade.php
@@ -4,33 +4,40 @@
 
 @section('content')
     <div class="bg-light p-4 rounded">
-        <h1>{{ ucfirst($role->name) }} Role</h1>
+        <h1>Vai trò: {{ ucfirst($role->name) }} </h1>
         <div class="lead">
 
         </div>
 
         <div class="container mt-4">
 
-            <h3>Assigned permissions</h3>
+            <h3>Quyền được chỉ định</h3>
 
-            <table class="table table-striped">
-                <thead>
-                    <th scope="col" width="20%">Name</th>
-                    <th scope="col" width="1%">Guard</th>
-                </thead>
-
-                @foreach ($rolePermissions as $permission)
-                    <tr>
-                        <td>{{ $permission->name }}</td>
-                        <td>{{ $permission->guard_name }}</td>
-                    </tr>
-                @endforeach
-            </table>
+            <div class="system-table table-responsive">
+                <table id="simpletable" class="table border text-nowrap customize-table mb-0 align-middle mb-3">
+                    <thead>
+                        <th scope="col" width="20%">Tên quyền</th>
+                        <th scope="col" width="1%">Vị trí</th>
+                    </thead>
+                    @foreach ($rolePermissions as $permission)
+                        <tr>
+                            <td>{{ $permission->name }}</td>
+                            <td>{{ $permission->guard_name }}</td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
         </div>
-
+        <div class="mt-4" style="text-align: right;">
+            <a href="{{ route('roles.edit', $role->id) }}" class="btn btn-info">
+                <span class="icon-item-icon me-1"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings-code" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M11.482 20.924a1.666 1.666 0 0 1 -1.157 -1.241a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.312 .318 1.644 1.794 .995 2.697"></path><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path><path d="M20 21l2 -2l-2 -2"></path><path d="M17 17l-2 2l2 2"></path></svg></span>
+                Sửa
+            </a>
+            <a class="btn btn-primary btn-sm" href="{{ route('roles.index') }}" class="btn btn-default">
+                <span class="icon-item-icon"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-back-up" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 14l-4 -4l4 -4"></path><path d="M5 10h11a4 4 0 1 1 0 8h-1"></path></svg></span>    
+                Trở về
+            </a>
+        </div>
     </div>
-    <div class="mt-4">
-        <a href="{{ route('roles.edit', $role->id) }}" class="btn btn-info">Edit</a>
-        <a href="{{ route('roles.index') }}" class="btn btn-default">Back</a>
-    </div>
+    
 @endsection
