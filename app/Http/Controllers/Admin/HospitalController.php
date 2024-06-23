874a58b16934836b42b51e8ee5f8226c7155a4c2
Ngô Tâm <155130804+TamAIRC@users.noreply.github.com>
2024-06-23 16:01:44 +0700

    Format code and move health_report from Hospital to Physical Controller

--- app/Http/Controllers/Admin/HospitalController.php
+++ app/Http/Controllers/Admin/HospitalController.php
@@ -3,15 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Services\HospitalService;
-use App\Services\HospitalTypeService;
-use App\Services\UserHospitalService;
 use App\Services\CompanyService;
 use App\Services\ContractService;
+use App\Services\HospitalService;
+use App\Services\HospitalTypeService;
 use App\Services\PeriodService;
-use App\Services\HealthReportService;
-use App\Services\PeriodDetailService;
-
+use App\Services\UserHospitalService;
 use Illuminate\Http\Request;
 
 class HospitalController extends Controller
@@ -19,28 +16,22 @@ class HospitalController extends Controller
     protected $hospitalService;
     protected $hospitalTypeService;
     protected $userHospitalService;
-    protected $healthReportService;
     protected $companyService;
     protected $periodService;
     protected $contractService;
-    protected $periodDetailService;
 
-    
     public function __construct(
         HospitalService $hospitalService,
         HospitalTypeService $hospitalTypeService,
         UserHospitalService $userHospitalService,
-        CompanyService $companyService, 
-        PeriodService $periodService, 
+        CompanyService $companyService,
+        PeriodService $periodService,
         ContractService $contractService,
-        HealthReportService $healthReportService,
-        PeriodDetailService $periodDetailService
     ) {
         $this->hospitalService = $hospitalService;
         $this->hospitalTypeService = $hospitalTypeService;
         $this->userHospitalService = $userHospitalService;
         $this->companyService = $companyService;
-        $this->healthReportService = $healthReportService;
         $this->periodService = $periodService;
         $this->contractService = $contractService;
 
@@ -64,23 +55,11 @@ class HospitalController extends Controller
         if (!isset($params['contract'])) {
             $params['contract'] = $contractList->first() ? $contractList->first()->id : 0;
         }
-        
+
         $hospitalList = $this->hospitalService->getHospital();
         $hospitalTypeList = $this->hospitalTypeService->getHospitalType();
         $userHospitalList = $this->userHospitalService->getUserList();
         $hospitalContractList = $this->hospitalService->getHospitalContract();
-        return view('admin.hospital.index', compact(['hospitalList' , 'hospitalTypeList', 'userHospitalList', 'companyList', 'periodList', 'contractList','hospitalContractList']));
-    }
-    public function healthReport(Request $request)
-    {
-        $params = $request->query();
-        if (!isset($params['time_range'])) {
-            $params['time_range'] = date('01/01/Y') . ' - ' . date('d/m/Y');
-        }
-        $companyList = $this->companyService->getCompanyActiveSortByOrder();
-        $periodList = $this->periodService->getPeriodActiveByCompany($companyList->first()->id);
-        $contractList = $this->contractService->getContractByPeriod($periodList->first()->id);
-        $healthReportList = $this->healthReportService->getHealthReportList($companyList->first()->id,$params['time_range'],$params);
-        return view('admin.hospital.health_report', compact(['healthReportList' , 'companyList', 'periodList', 'contractList']));
+        return view('admin.hospital.index', compact(['hospitalList', 'hospitalTypeList', 'userHospitalList', 'companyList', 'periodList', 'contractList', 'hospitalContractList']));
     }
 }
