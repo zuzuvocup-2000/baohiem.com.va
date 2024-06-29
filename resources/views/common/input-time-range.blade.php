4aa890da906c7743f67a0779ab8aeefa4a2df156
Ngô Tâm <155130804+TamAIRC@users.noreply.github.com>
2024-06-29 17:57:17 +0700

    fix: Fix error "undefined" startdate when change input

--- resources/views/common/input-time-range.blade.php
+++ resources/views/common/input-time-range.blade.php
@@ -1,8 +1,7 @@
 <div class="form-field">
     <label for="" class="mb-1">Thời gian hiệu lực từ:</label>
     <div class="input-group">
-        <input type="text" class="form-control daterange" id="dateInput"
-            name="time_range"
+        <input type="text" class="form-control daterange" id="dateInput" name="time_range"
             value="{{ request()->time_range }}">
         <span class="input-group-text">
             <i class="ti ti-calendar fs-5"></i>
@@ -15,7 +14,7 @@
     $(document).ready(function() {
         $('#periodSelectGeneral').change(function() {
             var selectedOption = $(this).find('option:selected');
-            var startDate = selectedOption.attr('data-time-start');
+            var startDate = selectedOption.attr('data-time-start') || getCurrentDate();
             var currentDate = getCurrentDate();
             var combinedDate = startDate + ' - ' + currentDate
             $('#dateInput').val(combinedDate);
