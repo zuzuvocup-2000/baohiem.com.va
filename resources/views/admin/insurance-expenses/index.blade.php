7cf91e15db4fb5a8e0152528d774417f320eed46
unknown <minhbrots2@gmail.com>
2024-07-25 01:54:44 +0700

    [upload]: vaccin table UI

--- resources/views/admin/insurance-expenses/index.blade.php
+++ resources/views/admin/insurance-expenses/index.blade.php
@@ -285,26 +285,115 @@
                                                     </td>
                                                     <td>
                                                         <input
-                                                            class="inputField form-control int create-insurance-amount_paid"
+                                                            class="form-control int create-insurance-amount_paid"
                                                             type="text" value="">
                                                     </td>
                                                     <td>
                                                         <input
-                                                            class="inputField form-control int create-insurance-expected_payment"
+                                                            class="form-control int create-insurance-expected_payment"
                                                             type="text" value="">
                                                     </td>
                                                     <td>
                                                         <input
-                                                            class="inputField form-control int create-insurance-rejected_amount"
+                                                            class="form-control int create-insurance-rejected_amount"
                                                             type="text" value="">
                                                     </td>
                                                     <td>
-                                                        <input class="inputField form-control create-insurance-note"
+                                                        <input class="form-control create-insurance-note"
                                                             type="text" value="">
                                                     </td>
                                                 </tr>
                                             </tbody>
                                         </table>
+                                    </div>
+                                    <div class="vaccination-container" style="display: none;">
+                                        <div class="row">
+                                            <div class="col-sm-12 col-md-6 mb-3">
+                                                <div class="form-group">
+                                                    <label class="d-inline-block">
+                                                        <h5>Loại chủng ngừa:</h5>
+                                                    </label>
+                                                    <div class="col-sm">
+                                                        <select class="form-select">
+                                                            <option value="1">
+                                                                Bại liệt
+                                                            </option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-12 col-md-6 mb-3">
+                                                <div class="form-group">
+                                                    <label class="d-inline-block">
+                                                        <h5>Tên chủng ngừa:</h5>
+                                                    </label>
+                                                    <div class="col-sm">
+                                                        <select class="form-select">
+                                                            <option value="1">
+                                                                Ngừa Bại liệt
+                                                            </option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-12">
+                                                <h5>Tên Vacxin: <span class="font-normal">IMOVAX POLIO</span></h5>
+                                            </div>
+                                        </div>
+                                        <div class="table-responsive">
+                                            <table
+                                                class="table system-table border text-nowrap customize-table mb-0 align-middle mb-3 table-content-pay">
+                                                <thead class="text-dark fs-4">
+                                                    <tr role="row">
+                                                        <th>
+                                                            <h6 class="fs-4 fw-semibold mb-0 text-center"
+                                                                style="width: 100px">Thao tác</h6>
+                                                        </th>
+                                                        <th>
+                                                            <h6 class="fs-4 fw-semibold text-center mb-0">STT</h6>
+                                                        </th>
+                                                        <th>
+                                                            <h6 class="fs-4 fw-semibold text-center mb-0">Tên lần tiêm
+                                                            </h6>
+                                                        </th>
+                                                        <th>
+                                                            <h6 class="fs-4 fw-semibold text-center mb-0">Số tháng cách lần đầu tiên
+                                                            </h6>
+                                                        </th>
+                                                        <th>
+                                                            <h6 class="fs-4 fw-semibold text-center mb-0">Số tháng nhắc lại
+                                                            </h6>
+                                                        </th>
+                                                        <th>
+                                                            <h6 class="fs-4 fw-semibold text-center mb-0">Ngày tiêm</h6>
+                                                        </th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr role="row">
+                                                        <td colspan="2" >
+                                                            @include('common/button-loading', ['class' => 'btn-create-company'])
+                                                        </td>
+                                                        <td>
+                                                            <input class="inputField form-control create-company-address" type="text" name="address"
+                                                                value="" />
+                                                        </td>
+                                                        <td>
+                                                            <input class="inputField form-control create-company-phone_number" type="text" name="phone_number"
+                                                                value="" />
+                                                        </td>
+                                                        <td>
+                                                            <input class="inputField form-control create-company-email" type="text" name="email"
+                                                                value="" />
+                                                        </td>
+                                                        <td>
+                                                            <input class="inputField form-control create-company-ceo_name" type="text" name="ceo_name"
+                                                                value="" />
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
                                     </div>
                                     <div class="row mb-3 align-items-center">
                                         <div class="col-sm-12 col-md-3">
