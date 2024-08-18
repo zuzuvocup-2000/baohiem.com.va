a7dd70011191eb47e85916324dcecfdadbc00b22
Ngô Tâm <155130804+TamAIRC@users.noreply.github.com>
2024-08-18 18:52:28 +0700

    update "Khám sức khỏe"

--- app/Http/Controllers/Admin/PhysicalController.php
+++ app/Http/Controllers/Admin/PhysicalController.php
@@ -49,6 +49,9 @@ class PhysicalController extends Controller
             $params['period'] = $periodList->first()->id;
         }
         $contractList = $this->contractService->getContractByPeriod($params['period']);
+        if (!isset($params['contract'])) {
+            $params['contract'] = $contractList->first()->id;
+        }
 
         //Khoảng thời gian kiểm tra
         if (!isset($params['time_range'])) {
@@ -61,7 +64,9 @@ class PhysicalController extends Controller
 
         return view('admin.physical.index', compact(['companyList', 'periodList', 'contractList', 'physicalList']));
     }
+
     // TODO
+    // Khamsuckhoedinhky.aspx
     public function periodic(Request $request)
     {
         $params = $request->query();
@@ -74,15 +79,34 @@ class PhysicalController extends Controller
             $params['period'] = $periodList->first()->id;
         }
         $contractList = $this->contractService->getContractByPeriod($params['period']);
+        if (!isset($params['contract'])) {
+            $params['contract'] = $contractList->first()->id;
+        }
 
         if (!isset($params['keyword'])) {
             $params['keyword'] = "";
         }
         // Kiểm tra thực hiện search hay load
-        $physicalList = (isset($params['submit']) and $params['submit'] == "search")
-        ? $this->physicalService->getPeriodicPhysical($params)
-        : $this->physicalService->show_customer($params);
-
+        if (isset($params['submit']) and $params['submit'] == "search") {
+            $keyword = trim($params['keyword']);
+            if ($keyword != "") {
+                if (strlen($keyword) > 5) {
+                    $physicalList = $this->physicalService->getPeriodicPhysical($keyword, $params);
+                } else {
+                    echo "<script>alert('Thông tin tìm kiếm có ít nhất 6 ký tự!');</script>";
+                }
+            } else {
+                echo "<script>alert('Vui lòng nhập thông tin tìm kiếm!');</script>";
+            }
+        } else {
+            $physicalList = $this->physicalService->show_customer($params['contract']);
+        }
+        // Cưu long joc 2 -20067
+        // Cưu long joc 2 (o) -20058
+        // truong son - 20059
+        // pvfcco - 49
+        // talisman -20066
+        // gas south - 20068
         return view('admin.physical.periodic', compact(['companyList', 'periodList', 'contractList', 'physicalList']));
     }
     public function detail()
